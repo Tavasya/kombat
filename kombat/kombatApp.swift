f31df762f480ebedff8d636b0e4af68e96a2daa3
Tavasya Ganpati <tavasyag@gmail.com>
2026-08-16 00:23:46 -0700

    first commit

---
+++ kombat/kombatApp.swift
@@ -0,0 +1,17 @@
+//
+//  kombatApp.swift
+//  kombat
+//
+//  Created by Tavasya Ganpati on 8/16/26.
+//
+
+import SwiftUI
+
+@main
+struct kombatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ kombatTests/kombatTests.swift
@@ -0,0 +1,17 @@
+//
+//  kombatTests.swift
+//  kombatTests
+//
+//  Created by Tavasya Ganpati on 8/16/26.
+//
+
+import Testing
+@testable import kombat
+
+struct kombatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
